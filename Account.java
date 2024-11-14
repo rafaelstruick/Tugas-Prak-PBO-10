1e8a91d4df695accf78be5426d06208b68e36199
rafaelstruick <deswitaprsdeibago@gmail.com>
2024-11-14 15:27:13 +0700

    Account.java

--- Account.java
+++ Account.java
@@ -1,9 +1,12 @@
 package com.atm.model;
 
+import java.util.Scanner;
+
 public class Account {
     private String accountNumber;
     private String pin;
     private double balance;
+    public static final double MINIMUM_BALANCE = 50000; // Saldo minimal penarikan
 
     public Account(String accountNumber, String pin, double balance) {
         this.accountNumber = accountNumber;
@@ -30,5 +33,29 @@ public class Account {
 
     public void debit(double amount) {
         this.balance -= amount;
+    }
+
+    public void changePin() { // Methode mengubah pin
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Masukkan PIN lama: ");
+        String oldPin = scanner.nextLine();
+
+        if (!this.pin.equals(oldPin)) {
+            System.out.println("PIN lama tidak valid.");
+            return;
+        }
+
+        System.out.print("Masukkan PIN baru: ");
+        String newPin1 = scanner.nextLine();
+        System.out.print("Konfirmasi PIN baru: ");
+        String newPin2 = scanner.nextLine();
+
+        if (!newPin1.equals(newPin2)) {
+            System.out.println("PIN baru tidak cocok.");
+            return;
+        }
+
+        this.pin = newPin1;
+        System.out.println("PIN berhasil diubah.");
     }
 }
